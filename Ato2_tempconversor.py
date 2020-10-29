15ef4313620852e8c6b72a0557df1b7d768bc74f
Michelson Lima <67869450+AsgardianoBR@users.noreply.github.com>
2020-10-29 10:01:05 -0300

    Delete Ato2_tempconversor.py

--- Ato2_tempconversor.py
+++
@@ -1,9 +0,0 @@
-x = int(input())
-temp = input("Deseja converter para Celsius [C] ou Fahrenheit [F]? \n")
-
-if temp == 'F'or temp=='f':
-    fah = ((x * 9) / 5) + 32
-    print(f"Está fazendo {fah:.2f} °F")
-elif temp == 'C' or temp=='c':
-    cel = ((x - 32) * 5) /9
-    print(f"Está fazendo {cel:.2f} °C")
