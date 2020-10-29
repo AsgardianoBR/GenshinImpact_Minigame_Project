e4b00e3edd1e9e43344396f7900f88aef0d1cdbc
Michelson Lima <67869450+AsgardianoBR@users.noreply.github.com>
2020-10-29 10:01:13 -0300

    Delete Ato3_precokm.py

--- Ato3_precokm.py
+++
@@ -1,8 +0,0 @@
-km = float(input())
-
-if km > 200:
-    preco = 0.45 * km
-    print("O valor da passagem é: {:.2f}".format(preco))
-else:
-    preco = 0.50 * km
-    print("O valor da passagem é: {:.2f}".format(preco))
